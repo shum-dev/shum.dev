a2770e7b3011441a898290e9e3e57e8a4b674a3d
Shum <89135509130@mail.ru>
2023-01-27 22:24:42 +0700

    v1 ready to deploy

---
+++ src/main.ts
@@ -0,0 +1,50 @@
+const hiddenSections = document.querySelectorAll(".hidden-section");
+const hiddenLR = document.querySelectorAll(".hidden-lr");
+
+const sectionsObserver = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((section) => {
+      if (section.isIntersecting) {
+        section.target.classList.add("shown-section");
+        sectionsObserver.unobserve(section.target);
+
+        setTimeout(() => {
+          section.target.classList.remove("hidden-section", "shown-section");
+        }, 2000);
+      }
+    });
+  },
+  {
+    rootMargin: "0px 0px -150px 0px",
+  }
+);
+hiddenSections.forEach((element) => sectionsObserver.observe(element));
+
+// Left To Right Animation
+const LRObserver = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((element) => {
+      if (element.isIntersecting) {
+        element.target.classList.add("shown-lr");
+
+        LRObserver.unobserve(element.target);
+
+        setTimeout(() => {
+          element.target.classList.remove("hidden-lr", "shown-lr");
+          element.target.classList.add(
+            "hover:shadow-card-up",
+            "hover:-translate-y-2",
+            "transition"
+          );
+        }, 500);
+      }
+    });
+  },
+  {
+    threshold: 0.6,
+  }
+);
+
+hiddenLR.forEach((element) => LRObserver.observe(element));
+
+export {};
